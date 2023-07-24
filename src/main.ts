fb58cf4978f3e8e404153a6a989c73f39a8f0c85
Olya Y <f_akora@mail.ru>
2023-07-24 22:58:48 +0300

    feat: add error messages and status codes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 }
 bootstrap();
