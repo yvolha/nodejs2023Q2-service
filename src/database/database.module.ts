8cc722855a3c81bf93610289cc856abfee4597d6
Olya Y <f_akora@mail.ru>
2023-07-24 20:34:49 +0300

    feat: add interfaces for user, album, artist, favs and track

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,1 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { Database } from './database';
 
+@Global()
+@Module({
+  providers: [Database],
+  exports: [Database],
+})
+export class DatabaseModule {}
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,1 +1,6 @@
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
+export class Database {
+  
+}
