448dbdb69a8d2a5181f60342782392b4fd85ad77
Olya Y <f_akora@mail.ru>
2023-07-24 15:54:13 +0300

    feat: create and separate UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { AlbumController } from './album/album.controller';
-import { ArtistController } from './artist/artist.controller';
-import { FavsController } from './favs/favs.controller';
-import { TrackController } from './track/track.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, AlbumController, ArtistController, FavsController, TrackController, UserController],
+  imports: [UserModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
