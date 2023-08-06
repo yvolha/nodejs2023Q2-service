37fb358bdff2276302dcb71b6533c41bfe746546
Olya <f_akora@mail.ru>
2023-08-06 15:45:56 +0300

    feat: add more models to prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
 import { FavsModule } from './favs/favs.module';
+import { PrismaModule } from './prisma-module/prisma.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { FavsModule } from './favs/favs.module';
     AlbumModule,
     ArtistModule,
     FavsModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
