b540d426aabdf99e7a32326c9264699381f6ff46
Olya <f_akora@mail.ru>
2023-08-20 23:17:29 +0300

    feat: start doing auth/login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/user.dto';
 import { UserService } from 'src/user/user.service';
+import { LoginUserDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,9 +10,7 @@ export class AuthService {
     return await this.userService.createOne(createUserDto);
   }
 
-  async login() {
-    console.log('logging in');
-  }
+  async login({ login, password }: LoginUserDto) {}
 
   async refreshToken() {
     console.log('refreshing');
